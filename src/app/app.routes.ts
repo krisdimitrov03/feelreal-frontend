ab4f2cd413c1530ae7cc6266f47a1f3c4f502c2d
victorrobotxt <vkoychev@uni-sofia.bg>
2024-06-07 18:06:56 +0300

    Update auth guard so that it is a function instead of class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { CalendarComponent } from './features/calendar/calendar.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -16,6 +16,6 @@ export const routes: Routes = [
     {
         path: 'calendar',
         component: CalendarComponent,
-        canActivate: [AuthGuard]
+        canActivate: [authGuard]
     }
 ];
--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,29 +1,27 @@
-// src/app/core/guards/auth.guard.ts
-import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
-import { AuthState } from '../../features/auth/store/state';
 import { selectIsAuthenticated } from '../../features/auth/store/selectors/auth.selectors';
+import { Observable, of } from 'rxjs';
+import { map, take, catchError } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthGuard implements CanActivate {
-  constructor(private store: Store<AuthState>, private router: Router) {}
+export const authGuard: CanActivateFn = (route, state) => {
+  const store = inject(Store);
+  const router = inject(Router);
 
-  canActivate(): Observable<boolean> {
-    return this.store.select(selectIsAuthenticated).pipe(
-      take(1),
-      map(isAuthenticated => {
-        if (isAuthenticated) {
-          return true;
-        } else {
-          this.router.navigate(['/login']);
-          return false;
-        }
-      })
-    );
-  }
-}
+  return store.select(selectIsAuthenticated).pipe(
+    take(1),
+    map(isAuthenticated => {
+      if (isAuthenticated) {
+        return true;
+      } else {
+        router.navigate(['/login']);
+        return false;
+      }
+    }),
+    catchError(() => {
+      router.navigate(['/login']);
+      return of(false);
+    })
+  );
+};
