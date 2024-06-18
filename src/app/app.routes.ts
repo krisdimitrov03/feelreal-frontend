3fbb5fe51ad283b58f75b3d370791c545ed93936
victorrobotxt <vkoychev@uni-sofia.bg>
2024-06-18 13:05:59 +0300

    Update authGuard to be function, not a class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
 import { RegisterComponent } from './features/auth/components/register/register.component';
 import { CalendarComponent } from './features/calendar/calendar.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { authGuard } from './core/guards/auth.guard';
 import { CreateEventComponent } from './features/calendar/create-event/create-event.component';
 
 export const routes: Routes = [
@@ -17,11 +17,11 @@ export const routes: Routes = [
     {
         path: 'calendar',
         component: CalendarComponent,
-        canActivate: [AuthGuard]
+        canActivate: [authGuard]
     },
     { 
         path: 'createEvent', 
         component: CreateEventComponent, 
-        canActivate: [AuthGuard]
+        canActivate: [authGuard]
     }
 ];
